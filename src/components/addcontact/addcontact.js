d366aea44418efe6c5830efc64d38b672dd313c4
ubonisrael <akpanudohubonisrael@gmail.com>
2023-02-07 05:58:59 +0100

    updates 02 addede new components

--- src/components/addcontact/addcontact.js
+++ src/components/addcontact/addcontact.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
+import React, {useState} from 'react'
+import { FaTimes, FaUser, FaUserAlt, FaUserCircle } from 'react-icons/fa'
 
 import './addcontact.css'
 
+const formFields = [
+    {label: 'First Name', name: 'firstname'},
+    {label: 'Middle Name', name: 'midname'},
+    {label: 'Surname', name: 'surname'},
+    {label: 'Telephone', name: 'phone'},
+    {label: 'Email', name: 'mail'},
+    {label: 'Gender', name: 'gender'},
+    {label: 'Address', name: 'address'},
+    {label: 'Description', name: 'desc'},
+]
 
 const Addcontact = () => {
+    const avatar = document.querySelector('.avatar')
+
+    //function that handles submitting the form ?? passed down from app
+    const submit = e => {
+        e.preventDefault()
+        console.log('contact added');
+    }
+
+    const [firstname, setFirstname] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [input, setInput] = useState('')
+
+    const [error, setError] = useState({firstname: '', phone: '', email: ''})
+
+
+    // functions that handle input change forms
+    const handleFirstname = e => {
+        setFirstname(e.target.value)
+    }
+    const handlePhone = e => {
+        setPhone(e.target.value)
+    }
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+    //other inputs
+    const handleInput = e => {
+        setInput(e.target.value)
+    }
   return (
     <div className='addcontact'>
         <div className='addcontact__container'>
@@ -12,22 +52,45 @@ const Addcontact = () => {
                 <FaTimes />
             </button>
             <form className='addcontact__form'>
-                <div addcontact__form__header>
+                <div className='addcontact__form__header'>
                     <h2>Add Contact</h2>
                 </div>
-                {/* <div addcontact__form__details></div> image display */}
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
-                <div addcontact__form__details></div>
+                <div className='addcontact__form__details-image-container'>
+                    {avatar 
+                    ? <img src='' alt='avatar' className='addcontact__form__details-image'/>
+                    : <span className='addcontact__form__details-avatar-icon'><FaUserCircle  /></span>}
+                    <label htmlFor='avatar'>Select Contact Image <input type='file' id='avatar' className='addcontact__form__details-avatar-picker' name='avatar' accept='image/*' /></label>
+                </div>
+                {formFields.map((field, i) => {
+                    return (
+                        <div key={field.name+i} className='addcontact__form__details-container'>
+                            <label htmlFor={field.name} className='addcontact__form__details-label'>{field.label} :</label>
+                            {field.name === 'desc'
+                            ? <textarea id='desc' className='addcontact__form__details-input desc' ></textarea>
+                            : field.name === 'gender'
+                            ? <select name='gender' id='gender'>
+                                <option value='male'>Male</option>
+                                <option value='female'>Female</option>
+                                <option value='other'>Other</option>
+                            </select>
+                            : <input id={field.name} className='addcontact__form__details-input'
+                                type={field.name === 'phone' ? 'number' : 'text'}
+                                onChange={field.name === 'firstname' ? handleFirstname
+                                : field.name === 'phone' ? handlePhone
+                                : field.name === 'email' ? handleEmail
+                                : handleInput}
+                                value={field.name === 'firstname' ? firstname
+                                    : field.name === 'phone' ? phone
+                                    : field.name === 'email' ? email
+                                    : input} />}
+                            {field.name === 'firstname' ? <span className='error'>{error.firstname}</span>
+                                    : field.name === 'phone' ? <span className='error'>{error.phone}</span>
+                                    : field.name === 'email' ? <span className='error'>{error.email}</span>
+                                    : null}
+                        </div>
+                    )
+                })}
+                <button type='submit' className='addcontact__form-btn' onClick={submit}>Add Contact</button>
             </form>
         </div>
     </div>
